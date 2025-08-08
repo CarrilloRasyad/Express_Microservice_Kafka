b0ac9a15eeca448e16ff77e4cc6885161875d355
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-08 15:19:29 +0700

    add: service unit test

--- src/services/__test__/catalog.service.test.ts
+++ src/services/__test__/catalog.service.test.ts
@@ -1,7 +1,30 @@
+import { ICatalogRepository } from "../../interface/catalogRepository.interface";
+import { CatalogRepository } from "../../repository/catalog.repository";
+import { MockCatalogRepository } from "../../repository/mockCatalog.repository";
+import { CatalogService } from "../catalog.service";
+
+
 describe("catalogService", () => {
 
-    test("example test", () => {
-        const a = 10;
-        expect(a).toEqual(10);
+    let repository: ICatalogRepository
+
+    beforeEach(() => {
+        repository = new MockCatalogRepository()
+    });
+
+    afterEach(() => {
+        repository = {} as MockCatalogRepository;
+    });
+
+    describe("createProduct", () => {
+        test("should create product", async() => {
+            const service = new CatalogService(repository);
+            const result = await service.createProduct({});
+            expect(result).toMatchObject()
+        })
+
+        test("should throw error with product already exists", () => {
+
+        })
     });
 });
