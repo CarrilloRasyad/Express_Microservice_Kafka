c4c3271eaa4dc64e42722a647aa2cd8bc527a4dd
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-08 14:21:46 +0700

    fix: package.json, catalog routes, and expressApp

--- src/api/catalog.routes.ts
+++ src/api/catalog.routes.ts
@@ -6,4 +6,16 @@ router.post("/product", async(req: Request, res: Response, next: NextFunction) =
     return res.status(201).json({})
 })
 
+// router.patch("/product", (req,res) => {
+// return res.json({});
+// })
+
+// router.delete("/", (req,res) => {
+// return res.json({});
+// })
+
+// router.get("/", (req,res) => {
+// return res.json({});
+// })
+
 export default router;
--- src/expressApp.ts
+++ src/expressApp.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 import catalogRouter from './api/catalog.routes'
 
-const PORT = process.env.PORT || 8000;
-
 const app = express();
 
 app.use(express.json())
 
 app.use("/", catalogRouter);
 
-app.listen(PORT, () => {
-    console.log("App Listen to Port: ", PORT);
-})
+
+export default app;
