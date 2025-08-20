2cbbc65db3f3f82e12875351ece07a6afb029353
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-20 09:37:45 +0700

    [patch]: cart service & cart routes

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,32 +1,37 @@
 import express, { NextFunction, Request, Response } from 'express';
+import * as service from '../service/cart.service';
 
 const router = express.Router();
 
 router.post(
     "/cart", 
     async(req: Request, res: Response, next: NextFunction) => {
-     return res.status(201).json({message: "create cart"});
+        const response = await service.CreateCart(req.body);
+        return res.status(201).json(response);
     }
 );
 
 router.get(
     "/cart",
     async(req: Request, res: Response, next: NextFunction) => {
-        return res.status(200).json({message: "get data cart"});
+        const response = await service.GetCart(req.body);
+        return res.status(200).json(response);
     }
 );
 
 router.patch(
     "/cart/:id",
     async(req: Request, res: Response, next: NextFunction) => {
-        return res.status(200).json({message: "update cart"})
+        const response = await service.EditCart(req.body);
+        return res.status(200).json(response);
     }
 );
 
 router.delete(
     "/cart/:id",
     async(req: Request, res: Response, next: NextFunction) => {
-        return res.status(200).json({message: "delete cart"})
+        const response = await service.DeleteCart(req.body);
+        return res.status(200).json(response);
     }
 );
 
---
+++ src/service/cart.service.ts
@@ -0,0 +1,15 @@
+export const CreateCart = async (input: any) => {
+    return { message: "created cart from service"};
+};
+
+export const GetCart = async (input: any) => {
+    return { message: "get cart from service"};
+};
+
+export const EditCart = async (input: any) => {
+    return { message: "update cart from service"};
+};
+
+export const DeleteCart = async (input: any) => {
+    return { message: "delete cart from service"};
+};
