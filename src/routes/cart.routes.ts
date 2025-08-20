5f2d868701071079145e10235ced002b95c5d51b
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-20 09:23:12 +0700

    make routes API for order and cart service

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -2,11 +2,33 @@ import express, { NextFunction, Request, Response } from 'express';
 
 const router = express.Router();
 
-router.post("/cart", async(req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({
+router.post(
+    "/cart", 
+    async(req: Request, res: Response, next: NextFunction) => {
+     return res.status(201).json({message: "create cart"});
+    }
+);
 
-    });
-});
+router.get(
+    "/cart",
+    async(req: Request, res: Response, next: NextFunction) => {
+        return res.status(200).json({message: "get data cart"});
+    }
+);
+
+router.patch(
+    "/cart/:id",
+    async(req: Request, res: Response, next: NextFunction) => {
+        return res.status(200).json({message: "update cart"})
+    }
+);
+
+router.delete(
+    "/cart/:id",
+    async(req: Request, res: Response, next: NextFunction) => {
+        return res.status(200).json({message: "delete cart"})
+    }
+);
 
 
 export default router
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,4 +1,34 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from 'express';
+
+const router = express.Router();
+
+router.post(
+    "/order", 
+    async(req: Request, res: Response, next: NextFunction) => {
+     return res.status(201).json({message: "create order"});
+    }
+);
+
+router.get(
+    "/order",
+    async(req: Request, res: Response, next: NextFunction) => {
+        return res.status(200).json({message: "get data order"});
+    }
+);
+
+router.get(
+    "/order/:id",
+    async(req: Request, res: Response, next: NextFunction) => {
+        return res.status(200).json({message: "get data order"});
+    }
+);
+
+router.delete(
+    "/order/:id",
+    async(req: Request, res: Response, next: NextFunction) => {
+        return res.status(200).json({message: "delete order"})
+    }
+);
 
 
-const app = express();
+export default router
