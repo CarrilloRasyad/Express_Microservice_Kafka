1439b2c26a523438269dbbb9cd37a855dfd9aa31
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-21 11:22:41 +0700

    add: configuration drizzle, schema, and migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ import { DB_URL } from "./src/config";
 export default defineConfig({
   schema: "./src/db/schema/*",
   out: "./src/db/migrations",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: DB_URL as string,
+    url: DB_URL as string,
   },
   verbose: true,
   strict: true,
