5064bd5474b70d6ea325a385f973370abafb81b8
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-26 10:22:53 +0700

    add package pino http, and class-validator

--- src/express-app.ts
+++ src/express-app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import orderRoutes from './routes/order.routes';
 import cartRoutes from './routes/cart.routes';
+import { HandleErrorWithLogger } from "./utils/error";
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,8 @@ app.use(cartRoutes);
 app.use("/", (req: Request, res: Response, _: NextFunction) => {
     return res.status(200).json({message: "I am healthy!"});
 });
+
+app.use(HandleErrorWithLogger);
 
 
 export default app;
