88ca4e1e36f5b656a7d03a0e4bc7db23f1aca2a4
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-26 10:32:32 +0700

    add handle error with logger

--- src/express-app.ts
+++ src/express-app.ts
@@ -2,11 +2,12 @@ import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import orderRoutes from './routes/order.routes';
 import cartRoutes from './routes/cart.routes';
-import { HandleErrorWithLogger } from "./utils/error";
+import { httpLogger, HandleErrorWithLogger } from "./utils";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(httpLogger)
 
 app.use(orderRoutes);
 app.use(cartRoutes);
