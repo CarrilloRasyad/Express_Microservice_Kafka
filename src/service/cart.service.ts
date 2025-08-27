0c59dc24c0c241fec9d124891364b981f2c6da8b
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-08-27 13:59:42 +0700

    change typo if stock < quantity

--- src/service/cart.service.ts
+++ src/service/cart.service.ts
@@ -1,13 +1,13 @@
 import { CartRequestInput } from "../dto/cartRequest.do";
 import { CartRepositoryType } from "../types/repository.type";
-import { logger } from "../utils";
+import { logger, NotFoundError } from "../utils";
 import { GetProductDetails } from "../utils/broker";
 
 export const CreateCart = async (input: CartRequestInput, repo: CartRepositoryType) => {
     const product = await GetProductDetails(input.productId);
     logger.info(product);
-    if(product.stock > input.qty) {
-        throw new Error("product is out of stock");
+    if(product.stock < input.qty) {
+        throw new NotFoundError("product is out of stock");
     }
     const data = await repo.create(input);
     return data;
