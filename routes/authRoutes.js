c0446009ff75e747e90a73ce71f9d1f69becb822
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-09-25 10:19:17 +0700

    feat: user login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -36,3 +36,30 @@ router.post(
 
 });
 
+router.post(
+"/login",
+    async(req, res) => {
+        const {email, password} = req.body;
+        const user = await db.query("SELECT * FROM users where email = $1", [
+            email
+        ]);
+
+        if(user.rows.length === 0) {
+            return res.status(404).json({ message: "users not found" });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        if(!validPassword) {
+            return res.status(400).json({message: "Invalid email and password"});
+        }
+
+        const token = generateToken({
+            id: user.rows[0].id,
+            email: user.rows[0].email,
+        });
+
+        return res.status(200).json({message: "Login successfull", token});
+        
+    }
+);
+
