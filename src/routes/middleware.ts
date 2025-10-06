e65f39dc8c8221c3e1aebe6876ea0e6de4895f66
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-10-06 09:08:58 +0700

    add: middleware routes

---
+++ src/routes/middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { ValidateUser } from "../utils";
+
+export const RequestAuthorizer = async(req: Request, res: Response, next: NextFunction) => {
+    console.log("RequestAuthorizer", req.headers.authorization);
+    try {
+        if(!req.headers.authorization) {
+            return res.status(403).json({ error: "UnAuthorized due to authorization token missing!"});
+        }
+        const userData = await ValidateUser(req.headers.authorization as string);
+        req.user = userData;
+    } catch (error) {
+        console.log("error", error);
+        return res.status(403).json({ error});
+    }
+};
