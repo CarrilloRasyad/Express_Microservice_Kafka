9fbcf2441242b24f3ec89c7c5c81ca27e96fe367
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-10-06 14:29:49 +0700

    temporary code not yet fixed

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,11 +1,31 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { MessageBroker } from '../utils';
-import { OrderEvent } from '../types';
+import { OrderEvent, OrderStatus } from '../types';
+import { RequestAuthorizer } from './middleware';
+// import * as service from "../service/order.service";
+import { OrderRepository } from '../repository/order.repository';
+import { CartRepository } from '../repository/cart.repository';
 
 const router = express.Router();
+const repo = OrderRepository;
+const cartRepo = CartRepository;
+
+// router.post(
+//     "/order",
+//     RequestAuthorizer,
+//     async(req: Request, res: Response, next: NextFunction) => {
+//         const user = req.user;
+//         if(!user) {
+//             next(new Error("user not found"));
+//             return;
+//         }
+//         const response = await service.CreateOrder(user.id, repo, cartRepo)
+//     }
+// )
 
 router.post(
-    "/order", 
+    "/order",
+    RequestAuthorizer, 
     async(req: Request, res: Response, next: NextFunction) => {
         await MessageBroker.publish({
             topic: "OrderEvents",
@@ -42,6 +62,16 @@ router.get(
         return res.status(200).json({message: "get data order"});
     }
 );
+
+// router.patch(
+//     "/order/:id",
+//     async(req: Request, res: Response, next: NextFunction) => {
+//         const orderId = parseInt(req.params.id);
+//         const status = req.body.status as OrderStatus;
+//         const response = await service.UpdateOrder(orderId, status, repo);
+//         return res.status(200).json(response);
+//     }
+// )
 
 router.delete(
     "/order/:id",
